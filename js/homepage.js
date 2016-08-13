819eda80830e69b947199050663bc7ef847f77ee
Mark Greenwood <mrkjgreenwood@gmail.com>
2016-08-13 11:17:51 -0700

    Changed object instantiation to constructor function; feature window now displays a random faux news story on load

--- js/homepage.js
+++ js/homepage.js
@@ -1,6 +1,44 @@
 /* Retrieve cycling news from Google */
 
-//var elNews = document.getElementById('news')
+function getRandomInt(min, max) {
+	min = Math.ceil(min);
+	max = Math.floor(max);
+	return Math.floor(Math.random() * (max-min+1)) + min;
+}
+
+function newsItem(headline, image_url, article_text) {
+	this.headline = headline;
+	this.image_url = image_url;
+	this.article_text = article_text;
+}
+
+var newsItemArray = [];
+
+newsItemArray.push(
+	new newsItem(
+			'Kristin Armstrong wins Cascade Classic time trial',
+			'images/K_Armstrong_TT_Finish.jpg',
+			'Text to follow.'
+		)
+	);
+
+newsItemArray.push(
+	new newsItem(
+			'Breakaway survives at PIR, Nevers wins',
+			'images/CoreysBreakaway.jpg',
+			'Five minutes into Tuesday night\'s race at PIR, four riders pulled away and established a break. The four riders took advantage of the pace easing up following a points sprint and accelerated away from the group. The breakaway established a lead of up to 45 seconds on the main field, which gradually came down to a 15 second gap on the last lap. With 200m to go, Corey Nevers attacked and beat the other three riders to the line just ahead of the main field.'
+		)
+	);
+
+var elNews = document.getElementById('news');
+var storyIndex = getRandomInt(0, newsItemArray.length-1);
+console.log("Fetching news story ", storyIndex);
+
+var news = '';
+news += '<img class="newspic" src="' + newsItemArray[storyIndex].image_url + '"><h1>' + newsItemArray[storyIndex].headline + '</h1><p>' +
+	newsItemArray[storyIndex].article_text + '</p>';
+
+/*
 var newsy_stuff = [
 	"Kristin Armstrong wins gold in Women's Time Trial", 
 	"Van Vleuten crashes in Women's Road Race",
@@ -14,38 +52,48 @@ for (i = 0; i < newsy_stuff.length; i++) {
 }
 
 newsHeadlines += "</ul>";
+*/
 
-//elNews.innerHTML = newsHeadlines;
+elNews.innerHTML = news;
 
 var elMarket = document.getElementById('market');
 
-var item1 = new Object();
-
-item1.item = 'Trek 5200 road bike';
-item1.sell_or_buy = 'Sell';
-item1.description = '21" frame, Ultegra components, good condition, carbon fiber frame/fork';
-item1.price = 1000;
-
-var itemArray = [];
-
-itemArray.push(item1);
-
-var item2 = new Object();
-item2.item = 'Autographed "Shut Up, Legs!" jersey';
-item2.sell_or_buy = 'Sell';
-item2.description = 'Euro-fit jersey with "Shut Up, Legs", autographed by The Jensie, Jens Voigt';
-item2.price = 200;
-
-itemArray.push(item2);
-
-var itemTable = "<table><tr><th colspan=3>Marketplace</th></tr>";
-itemTable += "<tr><th>Buy/Sell</th><th>Item</th><th>Price</th>";
-
-for (i = 0; i < itemArray.length; i++) {
-	itemTable += "<tr><td>" + itemArray[i].sell_or_buy + "</td><td>" + 
-		itemArray[i].item + "</td><td>" + "$" + itemArray[i].price + "</td></tr>";
+function mktItem (item, sell_or_buy, description, price) {
+	this.item = item;
+	this.sell_or_buy = sell_or_buy;
+	this.description = description;
+	this.price = price;
 }
 
-itemTable += "</table>";
+var mktItemArray = [];
 
-elMarket.innerHTML = itemTable;
+mktItemArray.push(
+	new mktItem(
+		'Trek 5200 carbon road bike', 
+		'Sell', 
+		'21" frame, Ultegra components, good condition, carbon fiber frame/fork', 
+		1000
+		)
+	);
+
+
+mktItemArray.push(
+	new mktItem(
+		'Autographed "Shut Up, Legs!" jersey', 
+		'Sell', 
+		'Euro-fit jersey with "Shut Up, Legs", autographed by The Jensie, Jens Voigt', 
+		300
+		)
+	);
+
+var mktItemTable = "<table><tr><th colspan=3>Marketplace</th></tr>";
+mktItemTable += "<tr><th>Buy/Sell</th><th>Item</th><th>Price</th>";
+
+for (i = 0; i < mktItemArray.length; i++) {
+	mktItemTable += "<tr><td>" + mktItemArray[i].sell_or_buy + "</td><td>" + 
+		mktItemArray[i].item + "</td><td>" + "$" + mktItemArray[i].price + "</td></tr>";
+}
+
+mktItemTable += "</table>";
+
+elMarket.innerHTML = mktItemTable;
