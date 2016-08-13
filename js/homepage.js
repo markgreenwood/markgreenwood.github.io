a2d279a10412aeda54bb902751dd32967b9cc0b4
Mark Greenwood <mrkjgreenwood@gmail.com>
2016-08-12 20:22:54 -0700

    Experimented with table formatting, made marketplace items into objects, moved news into feature section

--- js/homepage.js
+++ js/homepage.js
@@ -18,6 +18,8 @@ newsHeadlines += "</ul>";
 //elNews.innerHTML = newsHeadlines;
 
 var elMarket = document.getElementById('market');
+
+/*
 var mkt_items = [
 	'Selling: Trek 5200 carbon road bike',
 	'Selling: Trek 820 mountain bike',
@@ -33,3 +35,35 @@ for (i = 0; i < mkt_items.length; i++) {
 itemsForMkt += "</table>";
 
 elMarket.innerHTML = itemsForMkt;
+*/
+
+var item1 = new Object();
+
+item1.item = 'Trek 5200 road bike';
+item1.sell_or_buy = 'Sell';
+item1.description = '21" frame, Ultegra components, good condition, carbon fiber frame/fork';
+item1.price = 1000;
+
+var itemArray = [];
+
+itemArray.push(item1);
+
+var item2 = new Object();
+item2.item = 'Autographed "Shut Up, Legs!" jersey';
+item2.sell_or_buy = 'Sell';
+item2.description = 'Euro-fit jersey with "Shut Up, Legs", autographed by The Jensie, Jens Voigt';
+item2.price = 200;
+
+itemArray.push(item2);
+
+var itemTable = "<table><tr><th colspan=3>Marketplace</th></tr>";
+itemTable += "<tr><th>Buy/Sell</th><th>Item</th><th>Price</th>";
+
+for (i = 0; i < itemArray.length; i++) {
+	itemTable += "<tr><td>" + itemArray[i].sell_or_buy + "</td><td>" + 
+		itemArray[i].item + "</td><td>" + "$" + itemArray[i].price + "</td></tr>";
+}
+
+itemTable += "</table>";
+
+elMarket.innerHTML = itemTable;
--- js/marketplace.js
+++ js/marketplace.js
@@ -3,6 +3,7 @@
 var item = new Object();
 
 item.item = 'Trek 5200 road bike';
+item.sell_or_buy = 'Sell';
 item.description = '21" frame, Ultegra components, good condition, carbon fiber frame/fork';
 item.price = 1000;
 
@@ -11,8 +12,18 @@ var itemArray = [];
 itemArray.push(item);
 
 item.item = 'Autographed "Shut Up, Legs!" jersey';
+item.sell_or_buy = 'Sell';
 item.description = 'Euro-fit jersey with "Shut Up, Legs", autographed by The Jensie, Jens Voigt';
 item.price = 200;
 
 itemArray.push(item);
 
+var itemTable = "<table><tr><th colspan=3>Marketplace</th></tr>";
+itemTable += "<tr><th>Buy/Sell</th><th>Item</th><th>Price</th>";
+
+for (i = 0; i < itemArray.length; i++) {
+	itemTable += "<tr><td>" + itemArray[i].sell_or_buy + "</td><td>" + 
+		itemArray[i].item + "</td><td>" + itemArray[i].price + "</td></tr>";
+}
+
+itemTable += "</table>";
